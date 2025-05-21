dee308e0bce9411bfc7f7d508633175ab69021a0
almadoro <me@almadoro.dev>
2025-05-21 18:54:51 -0500

    feat(@react-just/node): automatically setting NODE_ENV to production

--- packages/node/src/bin.ts
+++ packages/node/src/bin.ts
@@ -4,6 +4,10 @@ import { program } from "commander";
 import http from "node:http";
 import { createHandleFunction as createRequestListener } from "./handle";
 
+// React specs NODE_ENV to be set to "production" when using a production
+// client build.
+if (!process.env.NODE_ENV) process.env.NODE_ENV = "production";
+
 program
   .requiredOption("-p, --port <port>", "Port to listen on")
   .argument("<app>", "App folder. Must contain a manifest.json file");
