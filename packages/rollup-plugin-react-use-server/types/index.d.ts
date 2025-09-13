84c8ffc6bd8a3833b70e5476155d0e53491be5b6
almadoro <me@almadoro.dev>
2025-09-13 12:50:48 +0200

    chore: removes unusued rollup-plugin-react-use-server package

--- packages/rollup-plugin-react-use-server/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from "rollup";
-
-export default function reactUseServer(): Plugin;
-
-declare module "rollup" {
-  interface CustomPluginOptions {
-    reactUseServer: never;
-  }
-}
