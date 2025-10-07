6c9f72aaa710f7cb53f423b770549035ef8273f4
almadoro <me@almadoro.dev>
2025-10-07 17:18:20 +0200

    test(react-just): updates use server directve tests

--- packages/react-just/__tests__/use-server/transform/directive.test.ts
+++ packages/react-just/__tests__/use-server/transform/directive.test.ts
@@ -1,59 +1,57 @@
-import baseTransform from "@/vite/use-server/transform";
+import transform, { TransformOptions } from "@/vite/use-server/transform";
 import Generator from "@/vite/use-server/transform/generator";
 import { generate } from "astring";
 import { builders } from "estree-toolkit";
 import fs from "node:fs/promises";
 import path from "node:path";
-import { parseAst } from "vite";
+import { parseAstAsync } from "vite";
 import { describe, expect, test } from "vitest";
 
 describe("'use server' module level directive", () => {
-  test("doesn't transform when there is no directive", async () => {
-    const { transformed, code, level } = await transform(
-      "directive/no-directive.js",
-    );
+  test("throws an error when there is no directive and doesn't modify the program", async () => {
+    const program = await getProgram("directive/no-directive.js");
 
-    expect(transformed).toBe(false);
-    expect(level).toBe(null);
+    expect(() => transform(program, OPTIONS)).toThrowError();
+
+    const code = generate(program);
+
     expect(code).toMatchSnapshot();
   });
 
   test("transforms when the directive is found at the top of the file", async () => {
-    const { transformed, code, level } = await transform(
-      "directive/valid-module-level.js",
-    );
+    const program = await getProgram("directive/valid-module-level.js");
 
-    expect(transformed).toBe(true);
-    expect(level).toBe("module");
+    transform(program, OPTIONS);
+
+    const code = generate(program);
+
     expect(code).toMatchSnapshot();
   });
 
-  test("doesn't transform when the directive is not at the top of the file", async () => {
-    const { transformed, code, level } = await transform(
-      "directive/invalid-module-level.js",
-    );
+  test("throws an error when the directive is not at the top of the file and doesn't modify the program", async () => {
+    const program = await getProgram("directive/invalid-module-level.js");
 
-    expect(transformed).toBe(false);
-    expect(level).toBe(null);
+    expect(() => transform(program, OPTIONS)).toThrowError();
+
+    const code = generate(program);
+
     expect(code).toMatchSnapshot();
   });
 });
 
-async function transform(filepath: string) {
+const OPTIONS: TransformOptions = {
+  generator: new Generator({
+    getRegisterArguments: ({ exportName }) => [builders.literal(exportName)],
+    registerServerReferenceSource: "react-just",
+  }),
+  treeshakeImplementation: false,
+};
+
+async function getProgram(filepath: string) {
   const code = await fs.readFile(
     path.resolve(import.meta.dirname, "fixtures", filepath),
     "utf-8",
   );
 
-  const ast = parseAst(code);
-
-  const { transformed, level } = baseTransform(ast, {
-    generator: new Generator({
-      getRegisterArguments: ({ exportName }) => [builders.literal(exportName)],
-      registerServerReferenceSource: "react-just",
-    }),
-    treeshakeImplementation: false,
-  });
-
-  return { transformed, code: generate(ast), level };
+  return parseAstAsync(code);
 }
